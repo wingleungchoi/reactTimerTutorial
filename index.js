2042de7253780d523895246825f2c8a9d463c730
wingleungchoi <wingleungchoi@gmail.com>
2016-06-26 21:59:59 +0800

    example 2

--- index.js
+++ index.js
@@ -1,84 +1,145 @@
 ;(function(){
-var TimerExample = React.createClass({
+// Example TimeCounter 1
+// var TimerExample = React.createClass({
 
-    getInitialState: function(){
+//     getInitialState: function(){
 
-        // This is called before our render function. The object that is 
-        // returned is assigned to this.state, so we can use it later.
+//         // This is called before our render function. The object that is 
+//         // returned is assigned to this.state, so we can use it later.
 
-        return { 
-          elapsed: 0,
-          counting: null
-        };
-    },
+//         return { 
+//           elapsed: 0,
+//           counting: null
+//         };
+//     },
 
-    componentDidMount: function(){
+//     componentDidMount: function(){
 
-        // componentDidMount is called by react when the component 
-        // has been rendered on the page. We can set the interval here:
+//         // componentDidMount is called by react when the component 
+//         // has been rendered on the page. We can set the interval here:
 
-        this.setState({counting: true});
-        this.timer = setInterval(this.tick, 50);
-    },
+//         this.setState({counting: true});
+//         this.timer = setInterval(this.tick, 50);
+//     },
 
-    componentWillUnmount: function(){
+//     componentWillUnmount: function(){
 
-        // This method is called immediately before the component is removed
-        // from the page and destroyed. We can clear the interval here:
+//         // This method is called immediately before the component is removed
+//         // from the page and destroyed. We can clear the interval here:
 
-        clearInterval(this.timer);
-    },
+//         clearInterval(this.timer);
+//     },
 
-    tick: function(){
+//     tick: function(){
 
-        // This function is called every 50 ms. It updates the 
-        // elapsed counter. Calling setState causes the component to be re-rendered
+//         // This function is called every 50 ms. It updates the 
+//         // elapsed counter. Calling setState causes the component to be re-rendered
 
-        this.setState({elapsed: new Date() - this.props.start});
-    },
+//         this.setState({elapsed: new Date() - this.props.start});
+//     },
 
-    stopTimer: function(){
-      clearInterval(this.timer);
-    },
+//     stopTimer: function(){
+//       clearInterval(this.timer);
+//     },
 
-    resumeTimer: function() {
-      this.timer = setInterval(this.tick, 50);
-    },
+//     resumeTimer: function() {
+//       this.timer = setInterval(this.tick, 50);
+//     },
 
-    toggleTimer: function() {
-      var counting = this.state.counting;
-      if (counting) {
-        this.stopTimer();
-      } else{
-        this.resumeTimer();
-      };
-      this.setState({counting: !counting});
-    },
+//     toggleTimer: function() {
+//       var counting = this.state.counting;
+//       if (counting) {
+//         this.stopTimer();
+//       } else{
+//         this.resumeTimer();
+//       };
+//       this.setState({counting: !counting});
+//     },
 
-    render: function() {
+//     render: function() {
         
-        var elapsed = Math.round(this.state.elapsed / 100);
+//         var elapsed = Math.round(this.state.elapsed / 100);
 
-        // This will give a number with one digit after the decimal dot (xx.x):
-        var seconds = (elapsed / 10).toFixed(1);
+//         // This will give a number with one digit after the decimal dot (xx.x):
+//         var seconds = (elapsed / 10).toFixed(1);
 
-        // var counting = this.state.counting + " ";
+//         // var counting = this.state.counting + " ";
 
-        var buttonString = this.state.counting ? 'Stop' : 'Resumt';
+//         var buttonString = this.state.counting ? 'Stop' : 'Resumt';
 
-        // Although we return an entire <p> element, react will smartly update
-        // only the changed parts, which contain the seconds variable.
+//         // Although we return an entire <p> element, react will smartly update
+//         // only the changed parts, which contain the seconds variable.
 
-        // return <p onClick={this.toggleTimer}>This example was started <b>{seconds} seconds</b> ago.</p>;
-        return <div>
-          <p>{seconds} Seconds</p>
-          <button onClick={this.toggleTimer}>{buttonString} Counting</button>
-        </div>;
-    }
-});
+//         // return <p onClick={this.toggleTimer}>This example was started <b>{seconds} seconds</b> ago.</p>;
+//         return <div>
+//           <p>{seconds} Seconds</p>
+//           <button onClick={this.toggleTimer}>{buttonString} Counting</button>
+//         </div>;
+//     }
+// });
 
 
-ReactDOM.render(
-    <TimerExample start={Date.now()}/>,
-    document.getElementById('container')
-);}());
+// ReactDOM.render(
+//     <TimerExample start={Date.now()}/>,
+//     document.getElementById('container')
+// );
+
+
+// Example 2. Navigation menu
+// var MenuExample = React.createClass({
+
+//     getInitialState: function(){
+//         return { focused: 0 };
+//     },
+
+//     clicked: function(index){
+
+//         // The click handler will update the state with
+//         // the index of the focused menu entry
+//         console.log(this);
+//         this.setState({focused: index});
+//     },
+
+//     render: function() {
+
+//         // Here we will read the items property, which was passed
+//         // as an attribute when the component was created
+
+//         var self = this;
+
+//         // The map method will loop over the array of menu entries,
+//         // and will return a new array with <li> elements.
+
+//         return (
+//             <div>
+//                 <ul>{ this.props.items.map(function(m, index){
+        
+//                     var style = '';
+        
+//                     if(self.state.focused == index){
+//                         style = 'focused';
+//                     }
+        
+//                     // Notice the use of the bind() method. It makes the
+//                     // index available to the clicked function:
+// // TODO why the following line will break the whole app, return <li className={style} onClick={self.clicked(index)}>{m}</li>;
+//                     return <li className={style} onClick={self.clicked.bind(self, index)}>{m}</li>;
+        
+//                 }) }
+                        
+//                 </ul>
+                
+//                 <p>Selected: {this.props.items[this.state.focused]}</p>
+//             </div>
+//         );
+
+//     }
+// });
+
+// ReactDOM.render(
+//     <MenuExample items={ ['Home', 'Services', 'About', 'Contact us'] } />,
+//     document.getElementById('container')
+// );
+
+
+}());

41419edaa1d38b238fb9cce9c931d1ebc03fdfb9
wingleungchoi <wingleungchoi@gmail.com>
2016-06-27 22:28:06 +0800

    before change

--- index.js
+++ index.js
@@ -1,145 +1,91 @@
+// http://tutorialzine.com/2014/07/5-practical-examples-for-learning-facebooks-react-framework/
+// http://andrewhfarmer.com/getting-started-tutorials/
 ;(function(){
-// Example TimeCounter 1
-// var TimerExample = React.createClass({
 
-//     getInitialState: function(){
+// This is more complex example that uses two components -
+// a service chooser form, and the individual services inside it.
 
-//         // This is called before our render function. The object that is 
-//         // returned is assigned to this.state, so we can use it later.
 
-//         return { 
-//           elapsed: 0,
-//           counting: null
-//         };
-//     },
+var ServiceChooser = React.createClass({
 
-//     componentDidMount: function(){
+    getInitialState: function(){
+        return { total: 0 };
+    },
 
-//         // componentDidMount is called by react when the component 
-//         // has been rendered on the page. We can set the interval here:
+    addTotal: function( price ){
+        this.setState( { total: this.state.total + price } );
+    },
 
-//         this.setState({counting: true});
-//         this.timer = setInterval(this.tick, 50);
-//     },
+    render: function() {
 
-//     componentWillUnmount: function(){
+        var self = this;
 
-//         // This method is called immediately before the component is removed
-//         // from the page and destroyed. We can clear the interval here:
+        var services = this.props.items.map(function(s){
 
-//         clearInterval(this.timer);
-//     },
+            // Create a new Service component for each item in the items array.
+            // Notice that I pass the self.addTotal function to the component.
 
-//     tick: function(){
+            return <Service name={s.name} price={s.price} active={s.active} addTotal={self.addTotal} />;
+        });
 
-//         // This function is called every 50 ms. It updates the 
-//         // elapsed counter. Calling setState causes the component to be re-rendered
+        return <div>
+                    <h1>Our services</h1>
+                    
+                    <div id="services">
+                        {services}
 
-//         this.setState({elapsed: new Date() - this.props.start});
-//     },
+                        <p id="total">Total <b>${this.state.total.toFixed(2)}</b></p>
 
-//     stopTimer: function(){
-//       clearInterval(this.timer);
-//     },
+                    </div>
 
-//     resumeTimer: function() {
-//       this.timer = setInterval(this.tick, 50);
-//     },
+                </div>;
 
-//     toggleTimer: function() {
-//       var counting = this.state.counting;
-//       if (counting) {
-//         this.stopTimer();
-//       } else{
-//         this.resumeTimer();
-//       };
-//       this.setState({counting: !counting});
-//     },
+    }
+});
 
-//     render: function() {
+
+var Service = React.createClass({
+
+    getInitialState: function(){
+        return { active: false };
+    },
+
+    clickHandler: function (){
+
+        var active = !this.state.active;
+
+        this.setState({ active: active });
         
-//         var elapsed = Math.round(this.state.elapsed / 100);
+        // Notify the ServiceChooser, by calling its addTotal method
+        this.props.addTotal( active ? this.props.price : -this.props.price );
 
-//         // This will give a number with one digit after the decimal dot (xx.x):
-//         var seconds = (elapsed / 10).toFixed(1);
+    },
 
-//         // var counting = this.state.counting + " ";
+    render: function(){
 
-//         var buttonString = this.state.counting ? 'Stop' : 'Resumt';
+        return  <p className={ this.state.active ? 'active' : '' } onClick={this.clickHandler}>
+                    {this.props.name} <b>${this.props.price.toFixed(2)}</b>
+                </p>;
 
-//         // Although we return an entire <p> element, react will smartly update
-//         // only the changed parts, which contain the seconds variable.
+    }
 
-//         // return <p onClick={this.toggleTimer}>This example was started <b>{seconds} seconds</b> ago.</p>;
-//         return <div>
-//           <p>{seconds} Seconds</p>
-//           <button onClick={this.toggleTimer}>{buttonString} Counting</button>
-//         </div>;
-//     }
-// });
+});
 
 
-// ReactDOM.render(
-//     <TimerExample start={Date.now()}/>,
-//     document.getElementById('container')
-// );
+var services = [
+    { name: 'Web Development', price: 300 },
+    { name: 'Design', price: 400 },
+    { name: 'Integration', price: 250 },
+    { name: 'Training', price: 220 }
+];
 
 
-// Example 2. Navigation menu
-// var MenuExample = React.createClass({
+// Render the ServiceChooser component, and pass the array of services
 
-//     getInitialState: function(){
-//         return { focused: 0 };
-//     },
-
-//     clicked: function(index){
-
-//         // The click handler will update the state with
-//         // the index of the focused menu entry
-//         console.log(this);
-//         this.setState({focused: index});
-//     },
-
-//     render: function() {
-
-//         // Here we will read the items property, which was passed
-//         // as an attribute when the component was created
-
-//         var self = this;
-
-//         // The map method will loop over the array of menu entries,
-//         // and will return a new array with <li> elements.
-
-//         return (
-//             <div>
-//                 <ul>{ this.props.items.map(function(m, index){
-        
-//                     var style = '';
-        
-//                     if(self.state.focused == index){
-//                         style = 'focused';
-//                     }
-        
-//                     // Notice the use of the bind() method. It makes the
-//                     // index available to the clicked function:
-// // TODO why the following line will break the whole app, return <li className={style} onClick={self.clicked(index)}>{m}</li>;
-//                     return <li className={style} onClick={self.clicked.bind(self, index)}>{m}</li>;
-        
-//                 }) }
-                        
-//                 </ul>
-                
-//                 <p>Selected: {this.props.items[this.state.focused]}</p>
-//             </div>
-//         );
-
-//     }
-// });
-
-// ReactDOM.render(
-//     <MenuExample items={ ['Home', 'Services', 'About', 'Contact us'] } />,
-//     document.getElementById('container')
-// );
+ReactDOM.render(
+    <ServiceChooser items={ services } />,
+    document.getElementById('container')
+);
 
 
 }());
